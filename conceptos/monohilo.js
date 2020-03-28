bd951f45bdde0c9718ce9e490f247c741b68936b
Diego Luna <diegofcolunalopez@gmail.com>
2020-03-27 18:16:01 -0600

    Monohilo: implicaciones en diseno y seguridad

---
+++ conceptos/monohilo.js
@@ -0,0 +1,20 @@
+// Node es multi ilos en el ejemlo de avajo primero ejecuta los "log" y luego va por el "setInterval"
+// 
+// para poner no andar en la terminal ponemos "node (nombre del archivo)"
+
+console.log("Hola mundo de node");
+
+let i = 0 ;
+
+// el "setInterval() " nos ayuda a tener el eventLoop simpre escuchando para simpre, y cada cuanto timpo va a ejecutar esto
+setInterval( function () {
+  console.log(i);
+  i++;
+
+  // if (i === 5){
+  //   var  a = 3 + z;
+  // }
+}, 1000); 
+
+console.log("Segunda instrucción");
+
