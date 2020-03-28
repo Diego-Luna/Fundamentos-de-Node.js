9b278ffdea76628f00a0f0d9c11c46db87b8d117
Diego Luna <diegofcolunalopez@gmail.com>
2020-03-28 11:41:46 -0600

    Variables de entorno

---
+++ conceptos/entorno.js
@@ -0,0 +1,16 @@
+
+// las variables de entor no las pongo en mayusculas
+
+
+let nombre = process.env.NOMBRE || "Sin nombre";
+let web = process.env.WEB || "no tengo web";
+
+require('dotenv').config();
+const lista = process.env.MI_LISTA.split(',');
+
+console.log(process.env.NAME);
+console.log(lista);
+
+
+console.log('Hola ' + nombre);
+console.log('Mi web es: ' + web);
