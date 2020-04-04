0db57281b78dae349aba36567134fb006175ec98
Diego Luna <diegofcolunalopez@gmail.com>
2020-04-04 14:59:52 -0600

    Construyendo modulos:Require e Import

---
+++ paquetes/modulos/ES6/modulo.mjs
@@ -0,0 +1,9 @@
+function saludar() {
+  console.log("Hola Mundo");
+  
+}
+
+export default {
+  saludar,
+  prop1: "soy un modulo experimental"
+};
---
+++ paquetes/modulos/indes.js
@@ -0,0 +1,6 @@
+// traer modulos:
+const modulo = require('./modulo');
+
+// ejecutar la funcion del modulo:
+console.log(modulo.prop1);
+modulo.saludar();
